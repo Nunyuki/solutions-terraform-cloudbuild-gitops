f6f1a7d023a2dbd2c255e69ceff0994b6beef266
Nunyuki <cdaielodie@gmail.com>
2025-01-29 11:13:36 +0000

    Update project IDs and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "vital-charger-424406-u2-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/prod/terraform.tfvars
+++ environments/prod/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="vital-charger-424406-u2"
